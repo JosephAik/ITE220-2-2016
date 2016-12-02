85477460933841382ccba1f7888486a31f47a019
JosephAik <kyaw.tun@students.stamford.edu>
2016-12-02 10:19:37 +0700

    Add files via upload

---
+++ Week3/js/shop.js
@@ -0,0 +1,6 @@
+var customerName = "Joseph";
+var price = 10;
+var quantity = 2;
+var TotlePrice = price * quantity;
+var CustomerElement = document.getElementById("a");
+CustomerElement.textContent = customerName;
