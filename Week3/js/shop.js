b7dcc36e6a2a5b04673bd3f8bdf56b33bb66401a
JosephAik <kyaw.tun@students.stamford.edu>
2016-12-02 14:50:14 +0700

    Add files via upload

--- Week3/js/shop.js
+++ Week3/js/shop.js
@@ -4,3 +4,5 @@ var quantity = 2;
 var TotlePrice = price * quantity;
 var CustomerElement = document.getElementById("a");
 CustomerElement.textContent = customerName;
+var PriceElement = document.getElementById("b");
+PriceElement.textContent = "$" + quantity * price;
